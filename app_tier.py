b079a3db9da4ddc69f1266d7831ebd2685f3cd16
tanmaysalunke <tanmay442000@gmail.com>
2024-10-20 18:23:24 -0700

    check new apptier

--- app_tier.py
+++ app_tier.py
@@ -11,8 +11,8 @@ ASU_ID = '1229850390'
 input_bucket_name = f'{ASU_ID}-in-bucket'
 output_bucket_name = f'{ASU_ID}-out-bucket'
 sqs = boto3.client('sqs', region_name='us-east-1')
-request_queue_url = f'https://sqs.us-west-2.amazonaws.com/442042549532/{ASU_ID}-req-queue'
-response_queue_url = f'https://sqs.us-west-2.amazonaws.com/442042549532/{ASU_ID}-resp-queue'
+request_queue_url = f'https://sqs.us-east-1.amazonaws.com/442042549532/{ASU_ID}-req-queue'
+response_queue_url = f'https://sqs.us-east-1.amazonaws.com/442042549532/{ASU_ID}-resp-queue'
 s3 = boto3.client('s3')
 
 def upload_image_to_s3(bucket_name, file_name, image_data):
@@ -36,11 +36,11 @@ def process_image(image_data):
 
     # Calling face_recognition.py as a subprocess
     process = subprocess.run(['python', 'face_recognition.py', temp_file_path], capture_output=True, text=True)
-    os.unlink(temp_file_path)  # Clean up the temporary file
-
+    print(f"Face recognition output: {process.stdout}")
     if process.returncode == 0:
         return process.stdout.strip()
     else:
+        print(f"Error in face recognition: {process.stderr}")
         raise Exception(f"Error in face recognition: {process.stderr}")
 
 def poll_sqs():
@@ -50,8 +50,8 @@ def poll_sqs():
             for message in response['Messages']:
                 receipt_handle = message['ReceiptHandle']
                 body = json.loads(message['Body'])
-                file_name = body['fileName']
-                file_content = body['fileContent']
+                file_name = body['imageKey']
+                file_content = body['imageContent'] 
 
                 print(f"Processing image: {file_name}")
                 person_name = process_image(file_content)
@@ -59,6 +59,7 @@ def poll_sqs():
                     upload_image_to_s3(input_bucket_name, file_name, file_content)
                     upload_classification_to_s3(output_bucket_name, file_name.split('.')[0], person_name)
                     sqs.send_message(QueueUrl=response_queue_url, MessageBody=json.dumps({'fileName': file_name, 'classificationResult': person_name}))
+                    print(f"Sent message to response queue: {{'fileName': '{file_name}', 'classificationResult': '{person_name}'}}")
                     sqs.delete_message(QueueUrl=request_queue_url, ReceiptHandle=receipt_handle)
                     print(f"Processed {file_name}: {person_name}")
         else:
